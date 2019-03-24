32da8a3d36bc2508b8166ac1acd2bdc7301c7993
Viking2012 <viking2012@gmail.com>
2019-03-24 16:29:04 -0400

    cleaned up insertion sort loop

--- chapter2/main.go
+++ chapter2/main.go
@@ -6,8 +6,7 @@ import (
 
 func main() {
 	xs := []int{5, 2, 4, 6, 1, 3}
-	insertionSort(xs)
-	// fmt.Println(insertionSort(xs))
+	fmt.Println(insertionSort(xs))
 }
 
 func insertionSortOld(x []int) []int {
@@ -25,14 +24,15 @@ func insertionSortOld(x []int) []int {
 	return x
 }
 
+// insertionSort will sort an array
 func insertionSort(x []int) []int {
-	i := 0             // needed to collect the last index of the downward shift
+	i := 0             // needed to collect the last index of the shift
 	for j := range x { // iterate over all the items in the array
 		key := x[j] // this is the "topmost" item in the stack
 		for i = j - 1; i >= 0 && key < x[i]; i-- {
-			x[i+1] = x[i]
+			x[i+1] = x[i] // shift greater values the up array
 		}
-		x[i+1] = key
+		x[i+1] = key // shift the initializing amount to the bottom of the array
 	}
 	return x
 }
