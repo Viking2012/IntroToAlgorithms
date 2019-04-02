219e78c7d9da2fbbf519d0343c805b8c421b3cec
Viking2012 <viking2012@gmail.com>
2019-04-02 19:55:54 -0400

    chapter 2

--- chapter2/main.go
+++ chapter2/main.go
@@ -6,33 +6,57 @@ import (
 
 func main() {
 	xs := []int{5, 2, 4, 6, 1, 3}
-	fmt.Println(insertionSort(xs))
+
+	insertionSorted := insertionSort(xs)
+	fmt.Println("insertionSorted:", insertionSorted)
+	// fmt.Println("raw values:", xs)
+	L, R := mergeSortOld(xs, 0, 3, 6)
+	fmt.Println("mergeSorted:", L, R)
 }
 
-func insertionSortOld(x []int) []int {
-	for j := range x {
-		key := x[j]
+func insertionSortOld(xs []int) (A []int) {
+	A = make([]int, len(xs))
+	copy(A, xs)
+	for j := range A {
+		key := A[j]
 		i := j - 1
-		for i >= 0 && key < x[i] {
-			fmt.Println("Replacing:", x[i+1], "with:", x[i])
-			x[i+1] = x[i]
+		for i >= 0 && key < A[i] {
+			fmt.Println("Replacing:", A[i+1], "with:", A[i])
+			A[i+1] = A[i]
 			i = i - 1
 		}
-		x[i+1] = key
+		A[i+1] = key
 	}
-	fmt.Println(x)
-	return x
+	fmt.Println(A)
+	return A
 }
 
 // insertionSort will sort an array
-func insertionSort(x []int) []int {
-	i := 0             // needed to collect the last index of the shift
-	for j := range x { // iterate over all the items in the array
-		key := x[j] // this is the "topmost" item in the stack
-		for i = j - 1; i >= 0 && key < x[i]; i-- {
-			x[i+1] = x[i] // shift greater values the up array
+func insertionSort(xs []int) (A []int) {
+	i := 0                   // needed to collect the last indexs of the shift
+	A = make([]int, len(xs)) // slice to contain the sorted array
+	copy(A, xs)              // fill the slice with the raw values
+	for j := range A {       // iterate over all the items in the array
+		key := A[j] // this is the "topmost" item in the stack
+		for i = j - 1; i >= 0 && key < A[i]; i-- {
+			A[i+1] = A[i] // shift greater values the up array
 		}
-		x[i+1] = key // shift the initializing amount to the bottom of the array
+		A[i+1] = key // shift the initializing amount to the bottom of the array
 	}
-	return x
+	return A
+}
+
+func mergeSortOld(xs []int, p, q, r int) (L, R []int) {
+	n1 := q - p
+	n2 := r - q
+	L = make([]int, n1)
+	R = make([]int, n2)
+	for i := range L {
+		L[i] = xs[p+i]
+	}
+	for j := range R {
+		R[j] = xs[q+j]
+	}
+
+	return L, R
 }
