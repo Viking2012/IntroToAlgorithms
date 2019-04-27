90608d912f216af4d557cd510e37877e9ff2af51
Viking2012 <viking2012@gmail.com>
2019-04-27 16:33:48 -0400

    heapsort

--- 2_Sorting_and_Order_Statistics/heapsort/heapsort.go
+++ 2_Sorting_and_Order_Statistics/heapsort/heapsort.go
@@ -1,8 +1,52 @@
 package heapsort
 
-// heapsort is expected to run in O(n lgn) time
-func heapsort(xs []int) (A []int) {
-	A = make([]int, len(xs))
-	copy(A, xs)
-	return A
+import (
+	su "github.com/Viking2012/IntroToAlgorithms/utils"
+)
+
+// HeapSort is expected to run in O(n lgn) time
+func HeapSort(data su.Interface) su.Interface {
+	a := data
+
+	n := a.Len()
+	heapSort(a, 0, n)
+
+	return a
+}
+
+// siftDown implements the heap property on data[lo, hi).
+// first is an offset into the array where the root of the heap lies.
+func siftDown(data su.Interface, lo, hi, first int) {
+	root := lo
+	for {
+		child := 2*root + 1
+		if child >= hi {
+			break
+		}
+		if child+1 < hi && data.Less(first+child, first+child+1) {
+			child++
+		}
+		if !data.Less(first+root, first+child) {
+			return
+		}
+		data.Swap(first+root, first+child)
+		root = child
+	}
+}
+
+func heapSort(data su.Interface, a, b int) {
+	first := a
+	lo := 0
+	hi := b - a
+
+	// Build heap with greatest element at top.
+	for i := (hi - 1) / 2; i >= 0; i-- {
+		siftDown(data, i, hi, first)
+	}
+
+	// Pop elements, largest first, into end of data.
+	for i := hi - 1; i >= 0; i-- {
+		data.Swap(first, first+i)
+		siftDown(data, lo, i, first)
+	}
 }
--- 2_Sorting_and_Order_Statistics/main.go
+++ 2_Sorting_and_Order_Statistics/main.go
@@ -1,7 +1,19 @@
 package main
 
-func main() {
-	toHeap := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+import (
+	"fmt"
 
-	sorted := heapsort.heapsort(xs)
+	"github.com/Viking2012/IntroToAlgorithms/2_Sorting_and_Order_Statistics/heapsort"
+
+	su "github.com/Viking2012/IntroToAlgorithms/utils"
+)
+
+func main() {
+	// xs := []int{4, 3, 2, 1}
+	toHeap := su.IntSlice([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
+	// fromHeap := toHeap
+
+	fromHeap := heapsort.HeapSort(toHeap)
+
+	fmt.Println(fromHeap)
 }
