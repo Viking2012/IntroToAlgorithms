4ffde1f93f49529e5403d514dbeb08bb2894dd8c
Viking2012 <viking2012@gmail.com>
2019-04-27 20:15:58 -0400

    quicksort

--- 2_Sorting_and_Order_Statistics/main.go
+++ 2_Sorting_and_Order_Statistics/main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 
 	"github.com/Viking2012/IntroToAlgorithms/2_Sorting_and_Order_Statistics/heapsort"
-
+	"github.com/Viking2012/IntroToAlgorithms/2_Sorting_and_Order_Statistics/quicksort"
 	su "github.com/Viking2012/IntroToAlgorithms/utils"
 )
 
 func main() {
-	// xs := []int{4, 3, 2, 1}
-	toHeap := su.IntSlice([]int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1})
-	// fromHeap := toHeap
+	// xs := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+	xs := []int{80, 12, 1, 57, 18, 57, 46, 3, 79, 36, 56, 44, 23, 55, 61, 89, 60, 1, 31, 91, 96, 4, 5, 79, 25, 7, 59, 38, 46, 71, 19, 5, 85, 96, 18, 11, 87, 40, 12, 81, 28, 35, 51, 1, 95, 16, 59, 5, 35, 9, 53, 12, 95, 20, 13, 12, 56, 72, 9, 4, 48, 50, 78, 64, 97, 13, 70, 25, 17, 71, 85, 50, 70, 19, 43, 13, 28, 70, 79, 54, 19, 63, 46, 17, 6, 56, 78, 36, 75, 41, 37, 62, 21, 85, 95, 87, 44, 22, 5, 44}
+	c := make([]int, len(xs))
+	fmt.Println("Original", xs)
 
-	fromHeap := heapsort.HeapSort(toHeap)
+	copy(c, xs)
+	toSort := su.IntSlice(c)
+	fmt.Println("ToSort  ", toSort)
+	fromHeap := heapsort.HeapSort(toSort)
+	fmt.Println("Heaped  ", fromHeap)
 
-	fmt.Println(fromHeap)
+	copy(c, xs)
+	toSort = su.IntSlice(c)
+	fmt.Println("ToSort  ", toSort)
+	fromQuick := quicksort.Quicksort(toSort)
+	fmt.Println("Quicked ", fromQuick)
 }
---
+++ 2_Sorting_and_Order_Statistics/quicksort/quicksort.go
@@ -0,0 +1,51 @@
+package quicksort
+
+import (
+	su "github.com/Viking2012/IntroToAlgorithms/utils"
+)
+
+// Quicksort is expected to run in O(nlogn) time, although it's
+// worst case is actually O(n^2).
+func Quicksort(A su.Interface) su.Interface {
+	B := A
+
+	n := A.Len()
+	quicksort(B, 0, n-1)
+
+	// for left, right := 0, B.Len()-1; left < right; left, right = left+1, right-1 {
+	// 	B.Swap(left, right)
+	// }
+	return B
+}
+
+func partition(A su.Interface, p, r int) int {
+	// fmt.Printf("Paritioning at A[%d]\n", r)
+	x := A.Get(r)
+	// fmt.Printf("Paritioning at A[%d] and got %d\n", r, x)
+	i := p
+	// fmt.Println("Partitioning and setting i to ", i)
+
+	for j := p; j < r; j++ {
+		y := A.Get(j)
+		// fmt.Printf("Checking if %d:%d is larger than %d:%d\n", i, x, j, y)
+		if y <= x {
+			// fmt.Printf("Swapped %d:%d with %d:%d\n", i, A.Get(i), j, A.Get(j))
+			A.Swap(i, j)
+			// fmt.Println("now have", A)
+			i++
+		}
+	}
+	A.Swap(i, r)
+	return i
+}
+
+func quicksort(A su.Interface, p, r int) {
+	if p < r {
+		// fmt.Println("partitioning at ", p, r)
+		q := partition(A, p, r)
+		// fmt.Println("quicksorting at ", p, q-1)
+		quicksort(A, p, q-1)
+		// fmt.Println("quicksorting at ", p, q)
+		quicksort(A, q, r)
+	}
+}
